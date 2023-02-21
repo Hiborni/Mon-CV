d3300cf2c31232e345b4a42949de6b2677f4d72d
cedric <wordzacke@gmail.com>
2023-02-21 12:23:12 +0100

    first commit

---
+++ CV.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8">
+    <title>CV</title>
+    <link rel="stylesheet" href="CV.css">
+</head>
+
+<body>
+    <header>
+        <h1>Mon curriculum vitae</h1>
+
+    </header>
+
+    <nav>
+        <label for="toggle"> ☰ </label>
+        <input type="checkbox" id="toggle">
+            <ul>
+                <li>
+                    <a href="CV.php" title="Accueil">Accueil</a>
+                </li>
+
+                <li>
+                    <a href="https://me-qr.com/fr/mobile/pdf/12118832" title="Mon curriculum vitae en pdf">Mon curriculum vitae en PDF</a>
+                </li>
+
+                <li>
+                    <a href="#" title="Me contacter">Me contacter</a>
+                </li>
+            </ul>
+    </nav>
+
+    <div class="ImageHG">
+        <img src="php.jpg" alt="php" title="Développer PHP">
+    </div>
+
+    <section class="texteHD">
+        <p>
+            Je suis un Développer PHP qui aime crée, inover et apprendre de nouvelle chose chaque jour <br>
+            j'aime decouvrir de nouvelle technologie surtout qu'elle evolue vite
+        </p>
+    </section>
+
+</body>
+
+</html>
